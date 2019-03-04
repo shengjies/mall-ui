7bf4b78e0dac3694a4d84bdc740219d4c444d4fc
zhou <1305219970@qq.com>
2019-03-04 11:32:44 +0800

    mall-ui 完成评论 部分模板

--- src/order/info/index.js
+++ src/order/info/index.js
@@ -15,7 +15,59 @@ class orderInfoView extends Component{
         return(
             <div style={{backgroundColor:"#fff"}}>
                 <div className='example-input'>
-                    订单中心
+                    <Row>
+                        <Col xs={24} sm={12} md={6} lg={4}>
+                            <Input placeholder="订单编号" onChange={(e)=>{
+                                
+                            }} />
+                        </Col>
+                        <Col xs={24} sm={12} md={6} lg={4}>
+                            <Input placeholder="产品" onChange={(e)=>{
+                                this.setState({name:e === undefined?'':e.target.value})
+                            }} />
+                        </Col>
+                        <Col xs={24} sm={12} md={6} lg={4}>
+                            <Input placeholder="客户名称" onChange={(e)=>{
+                                    
+                            }} />
+                        </Col>
+                        <Col xs={24} sm={12} md={6} lg={4}>
+                            <Input placeholder="订单状态" onChange={(e)=>{
+                                    
+                            }} />
+                        </Col>
+                        <Col xs={24} sm={12} md={6} lg={4}>
+                            <Input placeholder="时间" onChange={(e)=>{
+                                    
+                            }} />
+                        </Col>
+                        <Col xs={24} sm={12} md={6} lg={4}>
+                            <Input placeholder="采购状态" onChange={(e)=>{
+                                    
+                            }} />
+                        </Col>
+                        <Col xs={24} sm={12} md={6} lg={4}>
+                            <Input placeholder="物流状态" onChange={(e)=>{
+                                    
+                            }} />
+                        </Col>
+                        <Col xs={24} sm={12} md={6} lg={4}>
+                            <Input placeholder="手机号码" onChange={(e)=>{
+                                    
+                            }} />
+                        </Col>
+                        <Col xs={24} sm={12} md={6} lg={4}>
+                            <Input placeholder="国家" onChange={(e)=>{
+                                    
+                            }} />
+                        </Col>
+                        <Col xs={24} sm={12} md={8} lg={4}>
+                        <Button type="primary" icon="search" onClick={()=>{
+                            this.setState({page:1,pageSize:50});
+                            this.findProductInfo(0,50);
+                        }}>查询</Button>
+                        </Col>
+                    </Row>
                 </div>
             </div>
         )
